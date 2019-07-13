99ac92bbf6802fec506a6255307b33e52c902dfe
Darrel <darrel.hong@gmail.com>
2019-07-13 18:39:37 +0800

    Save as png

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Create sortable for each semester
 for (i = 1; i < 11; i++) {
-  let el = document.getElementById("simpleList" + i);
+  let el = document.getElementById('simpleList' + i);
   Sortable.create(el, {
     group: {
-      name: "shared",
+      name: 'shared',
       put: function(to) {
         return to.el.children.length < 7;
       }
@@ -14,22 +14,22 @@ for (i = 1; i < 11; i++) {
       */
     },
     animation: 150,
-    ghostClass: "ghost",
+    ghostClass: 'ghost',
     onAdd: function(evt) {
       handleAddEvent(evt);
     },
     onRemove: function(evt) {
-      if (evt.to.id === "buffer") {
-        evt.item.classList.remove("error");
+      if (evt.to.id === 'buffer') {
+        evt.item.classList.remove('error');
       }
     }
   });
 }
 // Create sortable for buffer
 Sortable.create(buffer, {
-  group: "shared",
+  group: 'shared',
   animation: 150,
-  ghostClass: "ghost",
+  ghostClass: 'ghost',
   onRemove: function(evt) {
     let moduleCode = evt.item.innerText.split(/\s/)[0];
     sessionStorage.setItem(moduleCode, evt.to.id.substring(10));
@@ -45,7 +45,7 @@ function handleAddEvent(evt) {
   sessionStorage.setItem(moduleCode, id);
   activeModules.push(moduleCode);
   if (!prereqV2(moduleCode)) {
-    evt.item.classList.add("error");
+    evt.item.classList.add('error');
   }
   recheckAll();
   console.log(activeModules);
@@ -53,20 +53,21 @@ function handleAddEvent(evt) {
 
 function handleAddToBufferEvent(evt) {
   let moduleCode = evt.item.innerText.split(/\s/)[0];
-  sessionStorage.setItem(moduleCode, "b");
+  sessionStorage.setItem(moduleCode, 'b');
   removeModule(moduleCode);
   console.log(activeModules);
-  evt.item.classList.remove("error");
+  evt.item.classList.remove('error');
   recheckAll();
 }
 
 // Recheck prereq
 function recheckAll() {
+  console.log('recheckAll');
   activeModules.forEach(activeMod => {
     if (!prereqV2(activeMod)) {
-      document.querySelector(`#${activeMod}`).classList.add("error");
+      document.querySelector(`#${activeMod}`).classList.add('error');
     } else {
-      document.querySelector(`#${activeMod}`).classList.remove("error");
+      document.querySelector(`#${activeMod}`).classList.remove('error');
     }
   });
 }
@@ -95,7 +96,7 @@ function prereqV2(module) {
 
 async function getModData(module) {
   const url = `https://api.nusmods.com/v2/2018-2019/modules/${module}.json`;
-  const cache = await caches.open("modcache");
+  const cache = await caches.open('modcache');
   let res = await cache.match(url);
   if (!res) {
     await cache.add(url);
@@ -108,14 +109,14 @@ async function getModData(module) {
   }
 }
 
-const colors = ["blue", "teal", "yellow", "orange", "red"];
+const colors = ['blue', 'teal', 'yellow', 'orange', 'red'];
 const moduleObjs = new Map();
 const activeModules = [];
 
 // Autocomplete module search
 async function getSearchData() {
-  const moduleListURL = "https://api.nusmods.com/v2/2018-2019/moduleList.json";
-  const cache = await caches.open("searchcache");
+  const moduleListURL = 'https://api.nusmods.com/v2/2018-2019/moduleList.json';
+  const cache = await caches.open('searchcache');
   let res = await cache.match(moduleListURL);
   if (!res) {
     await cache.add(moduleListURL);
@@ -129,7 +130,7 @@ async function getSearchData() {
 }
 
 getSearchData().then(ml => {
-  var input = document.getElementById("module-code");
+  var input = document.getElementById('module-code');
   autocomplete({
     input: input,
     fetch: function(text, update) {
@@ -141,10 +142,10 @@ getSearchData().then(ml => {
       update(suggestions);
     },
     render: function(item, value) {
-      const itemElement = document.createElement("div");
-      var regex = new RegExp(value, "gi");
+      const itemElement = document.createElement('div');
+      var regex = new RegExp(value, 'gi');
       var inner = item.moduleCode.replace(regex, function(match) {
-        return "<strong>" + match + "</strong>";
+        return '<strong>' + match + '</strong>';
       });
       itemElement.innerHTML = inner;
       itemElement.innerHTML += ` ${item.title}`;
@@ -157,7 +158,39 @@ getSearchData().then(ml => {
   });
 });
 
+async function persist() {
+  for (let i = 0; i < sessionStorage.length; i++) {
+    let moduleCode = sessionStorage.key(i);
+    let position = sessionStorage.getItem(sessionStorage.key(i));
+    getModData(moduleCode)
+      .then(data => {
+        moduleObjs.set(data.moduleCode, data);
+        return data;
+      })
+      .then(data => {
+        let color = getRandomItem(colors);
+        let card = returnCard(data, color);
+        if (position === 'b') {
+          addToBuffer(card);
+        } else {
+          activeModules.push(data.moduleCode);
+          addToPosition(card, position);
+          recheckAll();
+        }
+      });
+  }
+}
 
+persist().then(recheckAll());
+
+function addToPosition(htmlText, position) {
+  let el = document.createElement('div');
+  el.innerHTML = htmlText;
+  let list = `#simpleList${position}`;
+  console.log(list);
+  let positionEl = document.querySelector(list);
+  positionEl.insertBefore(el.firstChild, positionEl.firstChild);
+}
 
 function generateCourseCards(array) {
   array.forEach(mod => {
@@ -170,7 +203,7 @@ function generateCourseCards(array) {
         let color = getRandomItem(colors);
         let card = returnCard(data, color);
         addToBuffer(card);
-        sessionStorage.setItem(data.moduleCode, "b");
+        sessionStorage.setItem(data.moduleCode, 'b');
       });
   });
 }
@@ -182,7 +215,7 @@ function returnCard(data, color) {
     data.moduleCode
   }<br><span class="title">${data.title}</span></a>
   <span class='text-muted'>${data.moduleCredit} MCs</span>
-  <button type="button" class="close" aria-label="Close">
+  <button type="button" class="close" aria-label="Close" data-html2canvas-ignore>
   <span aria-hidden="true">&times;</span>
   </button>
   </div>`;
@@ -194,24 +227,18 @@ function getRandomItem(collection) {
 }
 
 // Global variables
-const bufferEl = document.querySelector("#buffer");
+const bufferEl = document.querySelector('#buffer');
 
 function addToBuffer(htmlText) {
-  let element = document.createElement("div");
+  let element = document.createElement('div');
   element.innerHTML = htmlText;
   bufferEl.insertBefore(element.firstChild, bufferEl.firstChild);
 }
-/*
-// Generate random cards
-for (i = 0; i < 5; i++) {
-  addToBuffer(generateRandomCard());
-}
-*/
 
 // Listen for '.close' events
-document.addEventListener("click", function(e) {
+document.addEventListener('click', function(e) {
   if (e.target.parentNode) {
-    if (e.target.parentNode.matches(".close")) {
+    if (e.target.parentNode.matches('.close')) {
       removeModule(e.target.parentNode.parentNode.id);
       e.target.parentNode.parentNode.remove();
       recheckAll();
@@ -220,10 +247,10 @@ document.addEventListener("click", function(e) {
 });
 
 //Add module button. Creates module card
-const moduleForm = document.querySelector("#module-form");
-moduleForm.addEventListener("submit", function(e) {
+const moduleForm = document.querySelector('#module-form');
+moduleForm.addEventListener('submit', function(e) {
   e.preventDefault();
-  let moduleCode = document.querySelector("#module-code").value;
+  let moduleCode = document.querySelector('#module-code').value;
   if (moduleCode) {
     getModData(moduleCode)
       .then(data => {
@@ -235,57 +262,57 @@ moduleForm.addEventListener("submit", function(e) {
         let color = getRandomItem(colors);
         let card = returnCard(data, color);
         addToBuffer(card);
-        sessionStorage.setItem(data.moduleCode, "b");
-        document.querySelector("#module-code").value = "";
-        document.querySelector("#buffer").scrollTo({
+        sessionStorage.setItem(data.moduleCode, 'b');
+        document.querySelector('#module-code').value = '';
+        document.querySelector('#buffer').scrollTo({
           top: 0,
-          behavior: "smooth"
+          behavior: 'smooth'
         });
       });
   }
 });
+
 // Course selector.
-const courseForm = document.querySelector("#course-select");
-courseForm.addEventListener("submit", function(e) {
+const courseForm = document.querySelector('#course-select');
+courseForm.addEventListener('submit', function(e) {
   e.preventDefault();
-  document.querySelector("#course-button").disabled = true;
-  let course = document.querySelector("#course").value;
+  document.querySelector('#course-button').disabled = true;
+  let course = document.querySelector('#course').value;
   console.log(course);
   switch (course) {
-    case "Computer Science":
+    case 'Computer Science':
       generateCourseCards(csCoreModules);
       break;
-    case "Business Analytics":
+    case 'Business Analytics':
       generateCourseCards(baCoreModules);
       break;
-    case "Information Systems":
+    case 'Information Systems':
       generateCourseCards(isCoreModules);
       break;
   }
 });
 
 function enableGenButton() {
-  document.querySelector("#course-button").disabled = false;
+  document.querySelector('#course-button').disabled = false;
 }
 
 // Toggle button 'active' class
-const btn1 = document.querySelector("#btn1");
-btn1.addEventListener("click", function() {
-  this.classList.toggle("active");
+const btn1 = document.querySelector('#btn1');
+btn1.addEventListener('click', function() {
+  this.classList.toggle('active');
 });
 
-const btn2 = document.querySelector("#btn2");
-btn2.addEventListener("click", function() {
-  this.classList.toggle("active");
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click', function() {
+  this.classList.toggle('active');
 });
 
 // Add year 5
 function addYear() {
-  document.getElementById("add-year").remove();
-  let yr5 = document.querySelectorAll("#yr5");
-  yr5.forEach(e => (e.style.display = "block"));
+  document.getElementById('add-year').remove();
+  let yr5 = document.querySelectorAll('#yr5');
+  yr5.forEach(e => (e.style.display = 'block'));
 }
-
 
 function generateRandomCard() {
   let color = getRandomItem(colors);
@@ -299,48 +326,73 @@ function generateRandomCard() {
           </div>`;
   return result;
 }
+
+let ssbutton = document.getElementById('btn-download');
+let box = document.querySelector('.container');
+ssbutton.addEventListener('click', function(e) {
+  html2canvas(document.body).then(function(canvas) {
+    console.log(canvas);
+    saveAs(canvas.toDataURL(), 'myModules.png');
+  });
+});
+
+function saveAs(uri, filename) {
+  var link = document.createElement('a');
+  if (typeof link.download === 'string') {
+    link.href = uri;
+    link.download = filename;
+    //Firefox requires the link to be in the body
+    document.body.appendChild(link);
+    //simulate click
+    link.click();
+    //remove the link when done
+    document.body.removeChild(link);
+  } else {
+    window.open(uri);
+  }
+}
 // prettier-ignore
 const csCoreModules = ['CS1010', 'CS1231', 'CS2030', 'CS2040', 'CS2100',
       'CS2103T', 'CS2105', 'CS2106', 'CS3230', 'IS1103', 'CS2101', 'ES2660', 'MA1521',
       'MA1101R', 'ST2334'];
 
 const baCoreModules = [
-  "BT1101",
-  "CS1010",
-  "EC1301",
-  "IS1103",
-  "MA1101R",
-  "MA1521",
-  "MKT1705X",
-  "BT2101",
-  "BT2102",
-  "CS2030",
-  "CS2040",
-  "IS2101",
-  "ST2334",
-  "BT3102",
-  "BT3102",
-  "BT3103",
-  "IS3103"
+  'BT1101',
+  'CS1010',
+  'EC1301',
+  'IS1103',
+  'MA1101R',
+  'MA1521',
+  'MKT1705X',
+  'BT2101',
+  'BT2102',
+  'CS2030',
+  'CS2040',
+  'IS2101',
+  'ST2334',
+  'BT3102',
+  'BT3102',
+  'BT3103',
+  'IS3103'
 ];
 
 const isCoreModules = [
-  "CS1010",
-  "CS1231",
-  "IS1103",
-  "CS2030",
-  "CS2040",
-  "CS2102",
-  "CS2105",
-  "IS2101",
-  "IS2102",
-  "IS2103",
-  "IS3103",
-  "IS3106",
-  "IS4100",
-  "IS4103",
-  "MA1301",
-  "MA1312",
-  "MA1521",
-  "ST2334"
+  'CS1010',
+  'CS1231',
+  'IS1103',
+  'CS2030',
+  'CS2040',
+  'CS2102',
+  'CS2105',
+  'IS2101',
+  'IS2102',
+  'IS2103',
+  'IS3103',
+  'IS3106',
+  'IS4100',
+  'IS4103',
+  'MA1301',
+  'MA1312',
+  'MA1521',
+  'ST2334'
 ];
